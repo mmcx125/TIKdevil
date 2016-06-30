273b204e0d066a75412fb46ae8b5fe92f12f5ccd
mmcx125 <mmcx125@gmx.com>
2016-06-30 16:29:45 -0600

    Added JSON define

--- source/utils.h
+++ source/utils.h
@@ -19,6 +19,8 @@ along with make_cdn_cia.  If not, see <http://www.gnu.org/licenses/>.
 #include <3ds.h>
 
 #define NUS_URL "http://ccs.cdn.c.shop.nintendowifi.net/ccs/download/"
+#define JSON_URL "https://3ds.titlekeys.com/json_enc"
+
 
 //MISC
 #ifdef __cplusplus
