6b7640199aaaf9dc2756fca34a5eba4000c2951b
Daniel Radtke <daniel.radtke@gmail.com>
2017-05-10 15:25:51 -0500

    Change domain for ticket website

--- source/utils.h
+++ source/utils.h
@@ -19,7 +19,7 @@ along with make_cdn_cia.  If not, see <http://www.gnu.org/licenses/>.
 #include <3ds.h>
 
 #define NUS_URL "http://ccs.cdn.c.shop.nintendowifi.net/ccs/download/"
-#define JSON_URL "https://3ds.titlekeys.com/json_enc"
+#define JSON_URL "http://3ds.titlekeys.gq/json_enc"
 
 // Title ID type definitions
 #define ESHOP_GAMEAPP "0000"
